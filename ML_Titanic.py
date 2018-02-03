d9d12d65a026f9ee47bcc36680e87df4f184f466
srazzak <suhail.razzak@gmail.com>
2018-02-02 21:39:31 -0500

    Added a bit more.

--- ML_Titanic.py
+++ ML_Titanic.py
@@ -1,3 +1,19 @@
 import sklearn as sk
 import numpy as np
 import pandas as pd
+import csv
+
+from sklearn import svm
+
+train_df = pd.read_csv('C:/Users/User/Documents/Python/Kaggle/Titanic/input/train.csv')
+test_df = pd.read_csv('C:/Users/User/Documents/Python/Kaggle/Titanic/input/test.csv')
+combine = [train_df, test_df]
+
+print(train_df.columns.values, '\n')
+
+desc_pClass = train_df[["Pclass", "Survived"]].groupby(['Pclass'], as_index=False).mean().sort_values(by='Survived', ascending=False)
+desc_Sex = train_df[["Sex", "Survived"]].groupby((['Sex']), as_index=False).mean().sort_values(by='Survived', ascending=False)
+
+print(desc_pClass, '\n')
+print(desc_Sex)
+
